653e89b57f2d4e74a3ce6ce8f4b51c489d1a0260
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-20 16:46:11 +0530

    Bussiness Section is on

--- src/components/Bussiness.jsx
+++ src/components/Bussiness.jsx
@@ -1,6 +1,17 @@
+import { features } from '../constants'
+import styles, {layout} from '../style'
+import Button from './Button'
+
 const Bussiness = () => {
   return (
-    <div>Bussiness</div>
+    <section id='features' className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business,<br className='sm:block hidden' /> we’ll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`} >With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+
+        <Button />
+      </div>
+    </section>
   )
 }
 export default Bussiness
