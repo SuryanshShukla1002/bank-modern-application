4aabd18b444cfa716a95917dada0b29d90176d35
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-21 16:31:14 +0530

    Billing section is on

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,6 +1,11 @@
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+import { apple, bill , google } from '../assets'
+import styles,{layout} from '../style'
+
+
+const Billing = () => (
+  <section id='product'>
+
+  </section>
+)
+
 export default Billing
