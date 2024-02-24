515ea2c7fda7be641ed190a9408627d3286a7c0d
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-24 23:56:53 +0530

    CTA section is on

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,18 @@
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+import styles from '../style'
+import Button from './Button'
+
+
+
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY}`}>
+    <div>
+      <h2>Let’s try our service now!</h2>
+      <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    </div>
+    <div>
+      <Button />
+    </div>
+
+  </section>
+)
 export default CTA
