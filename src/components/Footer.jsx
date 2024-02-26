fc18ef02776b6c74801663e61704709f5d1f5d92
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-26 23:38:47 +0530

    SocialMedia Icons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,34 @@ const Footer = () => (
         <img src={logo} alt="hook__bank" className='w-[266px] h-[72px] object-contain' />
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
       </div>
+
+      <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+        {footerLinks.map((footerlink) => (
+          <div key={footerlink.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
+            <h4 className='font-poppins font-medium text-[18px] text-white leading-[27px] '>
+              {footerlink.title}
+            </h4>
+            <ul className='list-none mt-4'>
+              {footerlink.links.map((link, index) => (
+                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerlink.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+      </div>
+    </div>
+
+    <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F345]'>
+      <p className='font-poppins font-normal text-[18px] text-white leading-[27px]'>2024 HooBank. All Rights Reserved.</p>
+      <div className='flex flex-row md:mt-0 mt-6'>
+        {socialMedia.map((social , index) => (
+          <img src={social.icon} alt={social.id} key={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length-1 ? 'm-3' : 'm-3' }`} />
+        ))}
+
+      </div>
     </div>
   </section>
 )
