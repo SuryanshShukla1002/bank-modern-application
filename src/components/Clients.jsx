1d09d8769e077f8ea519040ea818041d0f6d2f3a
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-28 16:38:03 +0530

    CardDeal Section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,17 +1,22 @@
-import styles from '../style'
-import { clients } from '../constants'
+import styles from "../style";
+import { clients } from "../constants";
 
 const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((clients) => (
-        <div key={clients.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] hover:text-white`}>
-          <img src={clients.logo} alt="client"
-          className='sm:w-[192px] w-[100px] object-contain cursor-pointer hover:bg-white' />
+        <div
+          key={clients.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] hover:text-white`}
+        >
+          <img
+            src={clients.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain cursor-pointer hover:bg-white"
+          />
         </div>
       ))}
     </div>
-
   </section>
-)
-export default Clients
+);
+export default Clients;
