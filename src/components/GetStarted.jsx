b9436a4f8946e69e07e4d8abb9a2b16a9427fe41
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-02-29 23:49:29 +0530

    Get Started portion

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,25 +1,28 @@
-import styles from '../style'
-import { arrowUp } from '../assets'
+import styles from "../style";
+import { arrowUp } from "../assets";
 
-const  GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-    <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+  >
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    >
       <div className={`${styles.flexStart} flex-row`}>
-        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-          <span className='text-gradient'>
-            Get 
-          </span>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="text-gradient">Get</span>
         </p>
-          <img src={arrowUp} alt='Arrow symbol' className='w-[23px] h-[23px] object-contain' />
+        <img
+          src={arrowUp}
+          alt="Arrow symbol"
+          className="w-[23px] h-[23px] object-contain"
+        />
       </div>
-      <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-          <span className='text-gradient'>
-            Started
-          </span>        
-        </p>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
     </div>
   </div>
-  
-  )
+);
 
-export default GetStarted
+export default GetStarted;
