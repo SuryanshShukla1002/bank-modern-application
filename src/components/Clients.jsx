e11345f398491091632882e003a442db6aa9f240
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-03-03 22:17:25 +0530

    bug fixed

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,5 @@
 import styles from "../style";
 import { clients } from "../constants";
-
 const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
